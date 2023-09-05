210f9b1f91533177ad436b14d6e8c1029f5ba819
Leonardo Corbella <leonardo.corbella7@gmail.com>
2023-09-05 16:14:21 -0300

    comentario en ColectivoTest

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -1,14 +1,15 @@
-<?php 
-
-namespace TrabajoSube;
-
-use PHPUnit\Framework\TestCase;
-
-class ColectivoTest extends TestCase{
-
-    public function testGetlinea(){
-        $cole = new Colectivo(103);
-        $this->assertEquals($cole->getLinea(), 103);
-    }
-    
-}
+#<?php 
+#
+#namespace TrabajoSube;
+#
+#use PHPUnit\Framework\TestCase;
+#
+#class ColectivoTest extends TestCase{
+#
+#    public function testGetlinea(){
+#        $cole = new Colectivo(103);
+#        $this->assertEquals($cole->getLinea(), 103);
+#    }
+#    
+#}
+#
