da790fe547798e4754397a4e03191559edd54a0b
ggiuliana <giulianagrieco12@gmail.com>
2023-10-15 19:22:13 -0300

    test tarjeta en desarrollo

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -6,10 +6,10 @@ use PHPUnit\Framework\TestCase;
 class TarjetaTest extends TestCase{
 
     public function testCargar(){
-        $tarjeta = new Tarjeta();
         $montosValidos = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
 
         foreach ($montosValidos as $monto){
+        $tarjeta = new Tarjeta();
         $tarjeta->cargarTarjeta($monto);
         $this->assertEquals($monto, $tarjeta->saldo);
         }   
