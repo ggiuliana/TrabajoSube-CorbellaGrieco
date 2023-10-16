0fe03ce3b9d4d3a4bfb3fb7b18cb014c658ed90a
LeonardoCorbella <leonardo.corbella7@gmail.com>
2023-10-16 00:15:41 -0300

    test de franquicias parciales

---
+++ tests/FranquiciasParcialesTest.php
@@ -0,0 +1,19 @@
+<?php 
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class FranquiciasParcialesTest extends TestCase{
+
+    public function testPuedePagaraConSaldo(){
+        $tarjeta = new FranquiciasParciales(200);
+        $this->assertTrue($tarjeta->cobrarBoleto());
+        $this->assertEquals(140, $tarjeta->saldo);
+    }
+
+    public function testPuedePagarConViajePlus(){
+        $tarjeta = new FranquiciasParciales(20);
+        $this->assertTrue($tarjeta->cobrarBoleto());
+        $this->assertEquals(-40, $tarjeta->saldo);
+    }
+}
