30a49b63e8951934f9646e7f55939a500bc615c6
LeonardoCorbella <leonardo.corbella7@gmail.com>
2023-10-15 23:33:36 -0300

    correcion en colectivo

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -3,10 +3,8 @@ namespace TrabajoSube;
 
 class Colectivo{
     protected $linea;
-    public $tarifa;
     
-    public function __construct($tarifa, $linea){
-        $this->tarifa = $tarifa;
+    public function __construct($linea){
         $this->linea = $linea;
     }
     
