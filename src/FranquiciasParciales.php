a3ae654b0798db8bf247ab020d081a2cc82d9816
LeonardoCorbella <leonardo.corbella7@gmail.com>
2023-10-16 14:57:30 -0300

    progresion en implementacion de boletos diarios

--- src/FranquiciasParciales.php
+++ src/FranquiciasParciales.php
@@ -15,23 +15,33 @@ class FranquiciasParciales extends Tarjeta{
     
     public function cobrarBoleto(){
         if ((($this->saldo-$this->tarifa) >= -211.84) && (time()-$this->ultimoBoleto >= 300)){
-            if($this->boletosDiarios>0){
-                $this->saldo-=$this->tarifa;
-                $this->ultimoBoleto = time();   
-                $this->boletosDiarios-=1;
-                return true;
+            if(date(d/m/Y, $this->ultimoBoleto) == date(d/m/Y)){
+
+                if($this->boletosDiarios>0){
+                    $this->saldo-=$this->tarifa;
+                    $this->ultimoBoleto = time();   
+                    $this->boletosDiarios-=1;
+                    return true;
+                }
+                else{
+                    $this->saldo-=$this->tarifa*2;
+                    $this->ultimoBoleto = time();   
+                    return true;
+                }
             }
             else{
-                $this->saldo-=$this->tarifa*2;
-                $this->ultimoBoleto = time();   
+                $this->boletosDiarios = 3;
+                $this->saldo-=$this->tarifa;
+                $this->ultimoBoleto = time();
                 return true;
+
             }
         }
 
-        else {
-             print 'Saldo insuficiente';
-            return false;
-        }
+            else {
+                print 'No es posible realizar el pago';
+                return false;
+            }
     
     }
 }
