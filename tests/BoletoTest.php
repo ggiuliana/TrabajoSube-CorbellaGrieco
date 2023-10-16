f167d0ba0dbce7b3f44ad06ac53a0f37321b86aa
ggiuliana <giulianagrieco12@gmail.com>
2023-10-16 11:18:09 -0300

    test boleto normal

---
+++ tests/BoletoTest.php
@@ -0,0 +1,14 @@
+<?php 
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class BoletoTest extends TestCase{
+    public function testBoletoNormal() {
+        $tarjeta = new Tarjeta(120);
+        $colectivo = new Colectivo(128);
+        $boleto = pagarCon($tarjeta);
+
+        $this->assertEquals($boleto->getTipo(), "normal");
+    }
+}
