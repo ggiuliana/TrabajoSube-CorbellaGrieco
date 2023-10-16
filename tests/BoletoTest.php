cb3f92b8fe4404616cd05a6517a8d7514c42423a
ggiuliana <giulianagrieco12@gmail.com>
2023-10-16 11:26:23 -0300

    tests tipos de boleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -11,4 +11,18 @@ class BoletoTest extends TestCase{
 
         $this->assertEquals($boleto->getTipo(), "normal");
     }
+    public function testFranquiciasCompletas() {
+        $tarjeta = new FranquiciasCompletas();
+        $colectivo = new Colectivo(128);
+        $boleto = $colectivo->pagarCon($tarjeta);
+
+        $this->assertEquals($boleto->getTipo(), "completa");
+    }
+    public function testFranquiciasParciales() {
+        $tarjeta = new FranquiciasParciales(60);
+        $colectivo = new Colectivo(128);
+        $boleto = $colectivo->pagarCon($tarjeta);
+
+        $this->assertEquals($boleto->getTipo(), "parcial");
+    }
 }
