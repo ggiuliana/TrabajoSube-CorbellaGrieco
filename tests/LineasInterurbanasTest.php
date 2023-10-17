9be44fb9557187d87cb0fad3001f99233327cee5
LeonardoCorbella <leonardo.corbella7@gmail.com>
2023-10-16 22:15:14 -0300

    tests boletos interurbanos fr

--- tests/LineasInterurbanasTest.php
+++ tests/LineasInterurbanasTest.php
@@ -7,21 +7,21 @@ class LineasInterurbanasTest extends TestCase{
     
     public function testBoletoInterurbano(){
         $tarjeta = new Tarjeta(184);
-        $colectivo = new Interurbano(35/9);
+        $colectivo = new LineasInterurbanas(35/9);
         $tarjeta->cobrarBoleto($colectivo->tarifa);
         $this->assertEquals(0, $tarjeta->saldo);
     }
 
     public function testMedioBoletoInterurbano(){
         $tarjeta = new FranquiciasParciales(92);
-        $colectivo = new Interurbano(35/9);
+        $colectivo = new LineasInterurbanas(35/9);
         $tarjeta->cobrarBoleto($colectivo->tarifa);
         $this->assertEquals(0, $tarjeta->saldo);
     }
 
     public function testBEGInterurbano(){
         $tarjeta = new FranquiciaBEG(92);
-        $colectivo = new Interurbano(35/9);
+        $colectivo = new LineasInterurbanas(35/9);
         $tarjeta->cobrarBoleto($colectivo->tarifa);
         $this->assertEquals(92, $tarjeta->saldo);
     }
