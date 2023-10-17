4c2fdd407ca66a30f462e845a43d2a33d646edc6
LeonardoCorbella <leonardo.corbella7@gmail.com>
2023-10-16 22:13:15 -0300

    tests boletos interurbanos

---
+++ tests/LineasInterurbanasTest.php
@@ -0,0 +1,28 @@
+<?php 
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class LineasInterurbanasTest extends TestCase{
+    
+    public function testBoletoInterurbano(){
+        $tarjeta = new Tarjeta(184);
+        $colectivo = new Interurbano(35/9);
+        $tarjeta->cobrarBoleto($colectivo->tarifa);
+        $this->assertEquals(0, $tarjeta->saldo);
+    }
+
+    public function testMedioBoletoInterurbano(){
+        $tarjeta = new FranquiciasParciales(92);
+        $colectivo = new Interurbano(35/9);
+        $tarjeta->cobrarBoleto($colectivo->tarifa);
+        $this->assertEquals(0, $tarjeta->saldo);
+    }
+
+    public function testBEGInterurbano(){
+        $tarjeta = new FranquiciaBEG(92);
+        $colectivo = new Interurbano(35/9);
+        $tarjeta->cobrarBoleto($colectivo->tarifa);
+        $this->assertEquals(92, $tarjeta->saldo);
+    }
+}
